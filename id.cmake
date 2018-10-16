d50149c13e138b03339704d8d5006d00a8538753
David Callu <callu.david@gmail.com>
2018-10-16 09:04:16 +0200

    chore(id): bump to version 1.7.4

--- id.cmake
+++ id.cmake
@@ -1,14 +1,14 @@
 set(package harfbuzz)
-set(version 1.8.8)
+set(version 1.7.4)
 
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
     VERSION ${version}-1
-    ABI 1.8
+    ABI 1.7
     )
 
 byd__package__download_info(${package}
-    URL "https://github.com/harfbuzz/harfbuzz/releases/download/1.8.8/harfbuzz-1.8.8.tar.bz2"
-    URL_HASH SHA1=be496408b2bb3dc794891e8b0e977cd75f02d71d
+    URL "https://github.com/harfbuzz/harfbuzz/releases/download/1.7.4/harfbuzz-1.7.4.tar.bz2"
+    URL_HASH SHA1=acaca9dd723516a06c7a237c9a6894101f736993
 )
